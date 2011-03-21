9b3fd29032965fcf9e2902212570e36df86d76d9
Tom Fennelly <tom.fennelly@gmail.com>
2011-03-21 20:52:35 +0000

    https://issues.jboss.org/browse/SWITCHYARD-163 Add support for using the build-time/plugin deployment scanners to SwitchYardTestCase

---
+++ tests/src/test/java/org/switchyard/component/model/ModelMergeTest.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+package org.switchyard.component.model;
+
+import org.junit.Test;
+import org.switchyard.config.model.Models;
+import org.switchyard.config.model.switchyard.SwitchYardModel;
+import org.switchyard.test.SwitchYardTestCase;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+/**
+ * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
+ */
+public class ModelMergeTest extends SwitchYardTestCase {
+
+    @Test
+    public void test() throws IOException {
+        SwitchYardModel model1 = loadSwitchYardModel(getResourceAsStream("switchyard_1.xml"));
+        SwitchYardModel model2 = loadSwitchYardModel(getResourceAsStream("switchyard_2.xml"));
+
+        // Merge and compare...
+        SwitchYardModel model3 = Models.merge(model1, model2, false);
+        compareToExpected(model3);
+
+        // Testing that merging the same model multiple times has no effect...
+        model3 = Models.merge(model3, model2, false);
+        model2 = loadSwitchYardModel(getResourceAsStream("switchyard_2.xml")); // re-read in case only works for instance equality
+        model3 = Models.merge(model3, model2, false);
+        compareToExpected(model3);
+    }
+
+    private void compareToExpected(SwitchYardModel mergedModel) throws IOException {
+        StringWriter stringWriter = new StringWriter();
+        mergedModel.write(stringWriter);
+//        System.out.println(stringWriter);
+        compareXMLToResource(stringWriter.toString(), "expected_merged_switchyard.xml");
+    }
+}
