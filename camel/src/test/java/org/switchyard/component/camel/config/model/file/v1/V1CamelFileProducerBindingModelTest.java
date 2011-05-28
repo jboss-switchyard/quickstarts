11c809ac091e9b401e254efd19d667b76d4a8a20
Mario Antollini <antollinim@gmail.com>
2011-05-27 21:44:44 -0300

    SWITCHYARD-207 Added missing License Headers

--- camel/src/test/java/org/switchyard/component/camel/config/model/file/v1/V1CamelFileProducerBindingModelTest.java
+++ camel/src/test/java/org/switchyard/component/camel/config/model/file/v1/V1CamelFileProducerBindingModelTest.java
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
  * site: http://www.fsf.org.
  */
+
 package org.switchyard.component.camel.config.model.file.v1;
 
 import java.io.InputStream;
