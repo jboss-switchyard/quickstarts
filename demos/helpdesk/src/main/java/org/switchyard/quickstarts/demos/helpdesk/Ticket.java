65ed764a2d3d0a7c2d7960a8b2a05810751296f3
David Ward <david@errantepiphany.com>
2011-06-29 12:24:49 -0400

    SWITCHYARD-174 https://issues.jboss.org/browse/SWITCHYARD-174

---
+++ demos/helpdesk/src/main/java/org/switchyard/quickstarts/demos/helpdesk/Ticket.java
@@ -0,0 +1,47 @@
+/* 
+ * JBoss, Home of Professional Open Source 
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved. 
+ * See the copyright.txt in the distribution for a 
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use, 
+ * modify, copy, or redistribute it subject to the terms and conditions 
+ * of the GNU Lesser General Public License, v. 2.1. 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details. 
+ * You should have received a copy of the GNU Lesser General Public License, 
+ * v.2.1 along with this distribution; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package org.switchyard.quickstarts.demos.helpdesk;
+
+/**
+ * @author David Ward &lt;<a href="mailto:dward@jboss.org">dward@jboss.org</a>&gt; (C) 2011 Red Hat Inc.
+ */
+public class Ticket {
+
+    private String _id;
+    private String _status;
+
+    public String getId() {
+        return _id;
+    }
+
+    public Ticket setId(String id) {
+        _id = id;
+        return this;
+    }
+
+    public String getStatus() {
+        return _status;
+    }
+
+    public Ticket setStatus(String status) {
+        _status = status;
+        return this;
+    }
+
+}
