b23e14af15810d826fd63c1cd1d3b5d4cc4c6650
David Ward <david@errantepiphany.com>
2011-06-29 12:24:49 -0400

    SWITCHYARD-174: https://issues.jboss.org/browse/SWITCHYARD-174 "Support for BPMN2 and jBPM5"

---
+++ demos/helpdesk/src/main/java/org/switchyard/quickstarts/demos/helpdesk/TicketManagementServiceBean.java
@@ -0,0 +1,64 @@
+/* 
+ * JBoss, Home of Professional Open Source 
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved. 
+ * See the copyright.txt in the distribution for a 
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use, 
+ * modify, copy, or redistribute it subject to the terms and conditions 
+ * of the GNU Lesser General Public License, v. 2.1. 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details. 
+ * You should have received a copy of the GNU Lesser General Public License, 
+ * v.2.1 along with this distribution; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package org.switchyard.quickstarts.demos.helpdesk;
+
+import org.switchyard.component.bean.Service;
+
+/**
+ * @author David Ward &lt;<a href="mailto:dward@jboss.org">dward@jboss.org</a>&gt; (C) 2011 Red Hat Inc.
+ */
+@Service(TicketManagementService.class)
+public class TicketManagementServiceBean implements TicketManagementService {
+
+    @Override
+    public TicketAck openTicket(Ticket ticket) {
+        log("opening ticket");
+        ticket.setStatus("opened");
+        return new TicketAck().setId(ticket.getId()).setReceived(true);
+    }
+
+    @Override
+    public void approveTicket(Ticket ticket) {
+        log("approving ticket");
+        ticket.setStatus("approved");
+    }
+
+    @Override
+    public void closeTicket(Ticket ticket) {
+        log("closing ticket");
+        ticket.setStatus("closed");
+    }
+
+    @Override
+    public void requestDetails(Ticket ticket) {
+        log("requesting details");
+        ticket.setStatus("requested");
+    }
+
+    @Override
+    public void rejectTicket(Ticket ticket) {
+        log("rejecting ticket");
+        ticket.setStatus("rejected");
+    }
+
+    private void log(String msg) {
+        System.err.println("********** " + getClass().getSimpleName() + ": " + msg + " **********");
+    }
+
+}
