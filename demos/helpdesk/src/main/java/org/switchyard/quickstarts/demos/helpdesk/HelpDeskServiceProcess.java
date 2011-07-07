14c072ea0053bd64d9c443769cba4c9c48b404bf
David Ward <david@errantepiphany.com>
2011-07-07 11:00:19 -0400

    SWITCHYARD-329: Add BPM annotations to mark methods as process action triggers

--- demos/helpdesk/src/main/java/org/switchyard/quickstarts/demos/helpdesk/HelpDeskServiceProcess.java
+++ demos/helpdesk/src/main/java/org/switchyard/quickstarts/demos/helpdesk/HelpDeskServiceProcess.java
@@ -18,14 +18,13 @@
  */
 package org.switchyard.quickstarts.demos.helpdesk;
 
-import org.switchyard.component.bpm.BPM;
-import org.switchyard.component.bpm.jbpm.CommandBasedWSHumanTaskHandler;
+import org.switchyard.component.bpm.Process;
+import org.switchyard.component.bpm.task.jbpm.CommandBasedWSHumanTaskHandler;
 
 /**
  * @author David Ward &lt;<a href="mailto:dward@jboss.org">dward@jboss.org</a>&gt; (C) 2011 Red Hat Inc.
  */
-@BPM(
-    processInterface = HelpDeskService.class,
-    taskHandlers = { CommandBasedWSHumanTaskHandler.class }
-)
-public final class HelpDeskServiceProcess {}
+@Process(
+    value=HelpDeskService.class,
+    taskHandlers={CommandBasedWSHumanTaskHandler.class})
+public interface HelpDeskServiceProcess {}
