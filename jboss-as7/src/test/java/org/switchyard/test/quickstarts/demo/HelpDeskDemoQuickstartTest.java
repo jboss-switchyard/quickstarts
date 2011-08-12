3768de937fa56ca583a9928b98d2e9c75bda6296
Tom Fennelly <tom.fennelly@gmail.com>
2011-08-12 18:34:19 +0100

    SWITCHYARD-392: Use BPM MixIn in AS7 release test HelpDeskDemoQuickstartTest
    https://issues.jboss.org/browse/SWITCHYARD-392

--- jboss-as7/src/test/java/org/switchyard/test/quickstarts/demo/HelpDeskDemoQuickstartTest.java
+++ jboss-as7/src/test/java/org/switchyard/test/quickstarts/demo/HelpDeskDemoQuickstartTest.java
@@ -18,31 +18,13 @@
  */
 package org.switchyard.test.quickstarts.demo;
 
-import java.net.Socket;
-import java.net.SocketException;
-import java.util.List;
-import java.util.Locale;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import org.drools.agent.impl.DoNothingSystemEventListener;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.jbpm.task.Status;
-import org.jbpm.task.User;
-import org.jbpm.task.query.TaskSummary;
-import org.jbpm.task.service.TaskClient;
-import org.jbpm.task.service.TaskServer;
-import org.jbpm.task.service.TaskService;
-import org.jbpm.task.service.TaskServiceSession;
-import org.jbpm.task.service.mina.MinaTaskClientConnector;
-import org.jbpm.task.service.mina.MinaTaskClientHandler;
-import org.jbpm.task.service.mina.MinaTaskServer;
-import org.jbpm.task.service.responsehandlers.BlockingTaskOperationResponseHandler;
-import org.jbpm.task.service.responsehandlers.BlockingTaskSummaryResponseHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.switchyard.test.ArquillianUtil;
+import org.switchyard.test.mixins.BPMMixIn;
 import org.switchyard.test.mixins.HTTPMixIn;
 
 /**
@@ -58,8 +40,8 @@ public class HelpDeskDemoQuickstartTest {
 
     @Test
     public void test() throws Exception {
-        HumanTaskServerHelper ht = new HumanTaskServerHelper();
-        if (ht.startTaskServer("Developer", "User")) {
+        BPMMixIn bpmMixIn = new BPMMixIn();
+        if (bpmMixIn.startTaskServer("Developer", "User")) {
             try {
                 HTTPMixIn httpMixIn = new HTTPMixIn();
 
@@ -72,10 +54,10 @@ public class HelpDeskDemoQuickstartTest {
 
                 boolean keepWorking = true;
                 while (keepWorking) {
-                    keepWorking = ht.completeTasksForUsers("Developer", "User");
+                    keepWorking = bpmMixIn.completeTasksForUsers("Developer", "User");
                 }
             } finally {
-                ht.stopTaskServer();
+                bpmMixIn.stopTaskServer();
             }
         }
     }
@@ -101,181 +83,4 @@ public class HelpDeskDemoQuickstartTest {
             + "        </helpdesk:openTicketResponse>\n"
             + "    </SOAP-ENV:Body>\n"
             + "</SOAP-ENV:Envelope>";
-
-    /**
-     * Human Task Server Helper. Based off the BPMMixIn from the jbpm-component.
-     * Helps with <a href="http://docs.jboss.org/jbpm/v5.0/userguide/ch.Human_Tasks.html">jBPM 5 Human Tasks</a>.
-     *
-     * @author Justin Wyer &lt;<a href="mailto:justin@lifeasageek.com">justin@lifeasageek.com</a>&gt; (C) 2011 Red Hat Inc.
-     */
-    class HumanTaskServerHelper {
-
-        /** The default host. */
-        public static final String DEFAULT_HOST = "127.0.0.1";
-        /** The default port. */
-        public static final int DEFAULT_PORT = 9123;
-        /** The Administrator user id. */
-        public static final String ADMINISTRATOR = "Administrator";
-        private TaskServer _server = null;
-        private TaskClient _client = null;
-        private boolean _clientConnected = false;
-
-        /**
-         * Starts the task server with the default host and port, and the specified user ids to pre-populate the database with.
-         * @param userIds the user ids to pre-populate the database with
-         * @return whether the task server started successfully
-         */
-        public boolean startTaskServer(String... userIds) {
-            return startTaskServer(DEFAULT_HOST, DEFAULT_PORT, userIds);
-        }
-
-        /**
-         * Starts the task server with the specified host and port, and the specified user ids to pre-populate the database with.
-         * @param host the task server host
-         * @param port the task server port
-         * @param userIds the user ids to pre-populate the database with
-         * @return whether the task server started successfully
-         */
-        public boolean startTaskServer(String host, int port, String... userIds) {
-            try {
-                boolean ready = false;
-                while (!ready) {
-                    Socket socket = null;
-                    try {
-                        socket = new Socket(host, port);
-                        if (socket.isConnected()) {
-                            Thread.sleep(1000);
-                        } else {
-                            ready = true;
-                        }
-                    } catch (SocketException se) {
-                        ready = true;
-                    } finally {
-                        if (socket != null) {
-                            socket.close();
-                            socket = null;
-                        }
-                    }
-                }
-                EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.jbpm.task");
-                TaskService taskService = new TaskService(emf, new DoNothingSystemEventListener());
-                TaskServiceSession taskServiceSession = taskService.createSession();
-                taskServiceSession.addUser(new User(ADMINISTRATOR));
-                for (String userId : userIds) {
-                    if (!ADMINISTRATOR.equals(userId)) {
-                        taskServiceSession.addUser(new User(userId));
-                    }
-                }
-                taskServiceSession.dispose();
-                _server = new MinaTaskServer(taskService, port, host);
-                new Thread(_server).start();
-                //_server.start();
-                return true;
-            } catch (Throwable t) {
-                System.err.println("*** Could not start task server: " + t.getMessage() + " ***");
-                return false;
-            }
-        }
-
-        /**
-         * Stops the task server.
-         */
-        public void stopTaskServer() {
-            if (_server != null) {
-                try {
-                    _server.stop();
-                } catch (Throwable t) {
-                    // just to keep checkstyle happy ("Must have at least one statement.")
-                    t.getMessage();
-                }
-                _server = null;
-            }
-        }
-
-        /**
-         * Starts the task client with the default host and port of the task server to connect to.
-         * @throws Exception if a problem occurred
-         */
-        public void startTaskClient() throws Exception {
-            startTaskClient(DEFAULT_HOST, DEFAULT_PORT);
-        }
-
-        /**
-         * Starts the task client with the specified host and port of the task server to connect to.
-         * @param host the task server host
-         * @param port the task server port
-         * @throws Exception if a problem occurred
-         */
-        public void startTaskClient(String host, int port) throws Exception {
-            if (_client == null) {
-                _client = new TaskClient(new MinaTaskClientConnector(HumanTaskServerHelper.class.getSimpleName(), new MinaTaskClientHandler(new DoNothingSystemEventListener())));
-            }
-            if (!_clientConnected) {
-                _client.connect(host, port);
-                _clientConnected = true;
-            }
-        }
-
-        /**
-         * Stops the task client.
-         */
-        public void stopTaskClient() {
-            if (_client != null) {
-                if (_clientConnected) {
-                    try {
-                        _client.disconnect();
-                    } catch (Throwable t) {
-                        // just to keep checkstyle happy ("Must have at least one statement.")
-                        t.getMessage();
-                    }
-                    _clientConnected = false;
-                }
-                _client = null;
-            }
-        }
-
-        /**
-         * Completes all tasks for the specified user ids, auto-connecting to the task server with the default host and port.
-         * @param userIds the user ids to complete the tasks for
-         * @return if there might be more user tasks to complete
-         * @throws Exception if a problem occurred
-         */
-        public boolean completeTasksForUsers(String... userIds) throws Exception {
-            return completeTasksForUsers(DEFAULT_HOST, DEFAULT_PORT, userIds);
-        }
-
-        /**
-         * Completes all tasks for the specified user ids, connecting to the task server with the specified host and port.
-         * @param host the task server host
-         * @param port the task server port
-         * @param userIds the user ids to complete the tasks for
-         * @return if there might be more user tasks to complete
-         * @throws Exception if a problem occurred
-         */
-        public boolean completeTasksForUsers(String host, int port, String... userIds) throws Exception {
-            boolean keepWorking = false;
-            startTaskClient(host, port);
-            for (String userId : userIds) {
-                BlockingTaskSummaryResponseHandler btsrh = new BlockingTaskSummaryResponseHandler();
-                _client.getTasksOwned(userId, Locale.getDefault().toString().replaceAll("_", "-"), btsrh);
-                List<TaskSummary> tasks = btsrh.getResults();
-                for (TaskSummary task : tasks) {
-                    if (Status.Completed.equals(task.getStatus())) {
-                        continue;
-                    }
-                    User user = task.getActualOwner();
-                    BlockingTaskOperationResponseHandler btorh = new BlockingTaskOperationResponseHandler();
-                    _client.start(task.getId(), user.getId(), btorh);
-                    btorh.waitTillDone(10000);
-                    btorh = new BlockingTaskOperationResponseHandler();
-                    _client.complete(task.getId(), user.getId(), null, btorh);
-                    btorh.waitTillDone(10000);
-                    keepWorking = true;
-                }
-            }
-            stopTaskClient();
-            Thread.sleep(1000);
-            return keepWorking;
-        }
-    }
 }
