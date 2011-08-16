7a053ac93afb6db55be161fb848fd974c0e57c3e
Keith Babo <kbabo@redhat.com>
2011-08-15 20:40:13 -0400

    SWITCHYARD-400 Add CDIMixIn to generated tests by default

--- tools/forge/plugin/src/main/resources/java/TestTemplate.java
+++ tools/forge/plugin/src/main/resources/java/TestTemplate.java
@@ -6,9 +6,10 @@ import org.switchyard.test.Invoker;
 import org.switchyard.test.ServiceOperation;
 import org.switchyard.test.SwitchYardRunner;
 import org.switchyard.test.SwitchYardTestCaseConfig;
+import org.switchyard.test.mixins.CDIMixIn;
 
 @RunWith(SwitchYardRunner.class)
-@SwitchYardTestCaseConfig(config=SwitchYardTestCaseConfig.SWITCHYARD_XML)
+@SwitchYardTestCaseConfig(mixins=CDIMixIn.class, config=SwitchYardTestCaseConfig.SWITCHYARD_XML)
 public class ${service.name}Test {
     
     @ServiceOperation("${service.name}")
