4f48c8291583416ad6de3a7c33b1975f0614f39c
objectiser <gary@pi4tech.com>
2011-10-05 12:03:13 +0100

    SWITCHYARD-484 initial two quickstarts for the BPEL component, updated to use the new soap binding config.

---
+++ bpel-service/loan_approval/src/test/java/org/switchyard/quickstarts/bpel/service/WebServiceTest.java
@@ -0,0 +1,38 @@
+package org.switchyard.quickstarts.bpel.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.switchyard.component.bean.config.model.BeanSwitchYardScanner;
+import org.switchyard.test.SwitchYardRunner;
+import org.switchyard.test.SwitchYardTestCaseConfig;
+import org.switchyard.test.mixins.CDIMixIn;
+import org.switchyard.test.mixins.HTTPMixIn;
+import org.switchyard.transform.config.model.TransformSwitchYardScanner;
+
+@RunWith(SwitchYardRunner.class)
+@SwitchYardTestCaseConfig(
+        config = SwitchYardTestCaseConfig.SWITCHYARD_XML,
+        scanners = {BeanSwitchYardScanner.class, TransformSwitchYardScanner.class},
+        mixins = {CDIMixIn.class, HTTPMixIn.class})
+public class WebServiceTest {
+
+    private HTTPMixIn httpMixIn;
+
+    @Test
+    public void loanApproval1() throws Exception {
+    	// Send a SOAP request and verify the SOAP reply is what we expected
+        httpMixIn.postResourceAndTestXML(
+        		"http://localhost:18001/loanService",
+        		"/xml/soap-loanreq1.xml",
+        		"/xml/soap-loanresp1.xml");
+    }
+
+    @Test
+    public void loanApproval2() throws Exception {
+    	// Send a SOAP request and verify the SOAP reply is what we expected
+        httpMixIn.postResourceAndTestXML(
+        		"http://localhost:18001/loanService",
+        		"/xml/soap-loanreq2.xml",
+        		"/xml/soap-loanresp2.xml");
+    }
+}
