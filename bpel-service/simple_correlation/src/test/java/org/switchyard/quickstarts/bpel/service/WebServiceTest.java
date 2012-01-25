9c1066c72966bcacb4aab028c290e2a797ea6717
Jeff Yu <jeff.yuchang@gmail.com>
2012-01-25 23:14:53 +1100

    RIFTSAW-471, Adding the simple_correlation example.

---
+++ bpel-service/simple_correlation/src/test/java/org/switchyard/quickstarts/bpel/service/WebServiceTest.java
@@ -0,0 +1,32 @@
+package org.switchyard.quickstarts.bpel.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.switchyard.component.bean.config.model.BeanSwitchYardScanner;
+import org.switchyard.test.SwitchYardRunner;
+import org.switchyard.test.SwitchYardTestCaseConfig;
+import org.switchyard.test.mixins.CDIMixIn;
+import org.switchyard.test.mixins.HTTPMixIn;
+import org.switchyard.transform.config.model.TransformSwitchYardScanner;
+
+@RunWith(SwitchYardRunner.class)
+@SwitchYardTestCaseConfig(
+        config = SwitchYardTestCaseConfig.SWITCHYARD_XML,
+        scanners = {BeanSwitchYardScanner.class, TransformSwitchYardScanner.class},
+        mixins = {CDIMixIn.class, HTTPMixIn.class})
+public class WebServiceTest {
+
+    private HTTPMixIn httpMixIn;
+
+    @Test
+    public void sayHello() throws Exception {
+    	// Send a SOAP request and verify the SOAP reply is what we expected
+        httpMixIn.postResourceAndTestXML(
+        		"http://localhost:18001/HelloGoodbyeService",
+        		"/xml/hello_request1.xml", "/xml/hello_response1.xml");
+	 
+        httpMixIn.postResourceAndTestXML(
+        		"http://localhost:18001/HelloGoodbyeService",
+        		"/xml/goodbye_request1.xml", "/xml/goodbye_response1.xml");	   			
+    }
+}
