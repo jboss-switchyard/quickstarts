b6174b9e00a2322308e2a67f30f443b69162f669
Tomohisa Igarashi <tm.igarashi@gmail.com>
2012-02-01 17:51:37 -0500

    SWITCHYARD-574 HTTPMixIn improvements
    - dump input/output message when _dumpMessages is true
    - added request header processing and response header evaluating

--- test/src/main/java/org/switchyard/test/mixins/HTTPMixIn.java
+++ test/src/main/java/org/switchyard/test/mixins/HTTPMixIn.java
@@ -19,6 +19,7 @@
 
 package org.switchyard.test.mixins;
 
+import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpConnectionManager;
 import org.apache.commons.httpclient.HttpMethod;
@@ -27,12 +28,15 @@ import org.apache.commons.httpclient.URIException;
 import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.switchyard.test.SwitchYardTestKit;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 
 /**
  * HTTP Test Mix In.
@@ -40,10 +44,14 @@ import java.io.UnsupportedEncodingException;
  * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
  */
 public class HTTPMixIn extends AbstractTestMixIn {
+    private static Logger _logger = Logger.getLogger(HTTPMixIn.class);
 
     private HttpClient _httpClient;
     private String _contentType = "text/xml";
-
+    private HashMap<String,String> _requestHeaders = new HashMap<String,String>();
+    private HashMap<String,String> _expectedHeaders = new HashMap<String,String>();
+    private boolean _dumpMessages = false;
+    
     /**
      * Set the content type.
      * <p/>
@@ -69,11 +77,16 @@ public class HTTPMixIn extends AbstractTestMixIn {
      * @return The HTTP response payload.
      */
     public String postString(String endpointURL, String request) {
+        if (_dumpMessages) {
+            _logger.info("Sending a POST request to [" + endpointURL + "]");
+            _logger.info("Request body:[" + request + "]");
+        }
+        
         PostMethod postMethod = new PostMethod(endpointURL);
         try {
             postMethod.setRequestEntity(new StringRequestEntity(request, _contentType, "UTF-8"));
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            _logger.error("Unable to set request entity", e);
         }
         try {
             return execute(postMethod);
@@ -103,6 +116,22 @@ public class HTTPMixIn extends AbstractTestMixIn {
      * @return The HTTP response payload.
      */
     public String postResource(String endpointURL, String requestResource) {
+        if (_dumpMessages) {
+            _logger.info("Sending a POST request to [" + endpointURL + "]");
+            InputStream input = getTestKit().getResourceAsStream(requestResource);
+            ByteArrayOutputStream tmp = new ByteArrayOutputStream();
+            byte[] data = new byte[4096];
+            int l = 0;
+            try {
+                while ((l = input.read(data)) >= 0) {
+                    tmp.write(data, 0, l);
+                }
+                _logger.info("Request body:[" + new String(tmp.toByteArray()) + "]");
+            } catch (IOException e) {
+                _logger.error("Unexpected Exception while reading request resource", e);
+            }
+        }
+        
         PostMethod postMethod = new PostMethod(endpointURL);
         InputStream requestStream = getTestKit().getResourceAsStream(requestResource);
 
@@ -147,20 +176,97 @@ public class HTTPMixIn extends AbstractTestMixIn {
             Assert.fail("HTTPMixIn not initialized.  You must call the initialize() method before using this MixIn");
         }
 
+        for (String key : _requestHeaders.keySet()) {
+            method.setRequestHeader(key, _requestHeaders.get(key));
+        }
+
+        if (_dumpMessages) {
+            for (Header header : method.getRequestHeaders()) {
+                _logger.info("Request header:[" + header.getName() + "=" + header.getValue() + "]");
+            }
+        }
+
+        String response = null;
         try {
             _httpClient.executeMethod(method);
-            return method.getResponseBodyAsString();
+            response = method.getResponseBodyAsString();
         } catch (Exception e) {
             try {
                 Assert.fail("Exception invoking HTTP endpoint '" + method.getURI() + "': " + e.getMessage());
             } catch (URIException e1) {
-                e1.printStackTrace();
+                _logger.error("Unexpected error", e1);
+                return null;
             }
         }
+            
+        if (_dumpMessages) {
+            for (Header header : method.getResponseHeaders()) {
+                _logger.info("Received response header:[" + header.getName() + "=" + header.getValue() + "]");
+            }
+            _logger.info("Received response body:[" + response + "]");
+        }
 
-        return null;
+        for (String key : _expectedHeaders.keySet()) {
+            Header actual = method.getResponseHeader(key);
+            Assert.assertNotNull("Checking response header:[" + key + "]", actual);
+            Assert.assertEquals("Checking response header:[" + key + "]", _expectedHeaders.get(key), actual.getValue());
+        }
+            
+        return response;
     }
 
+    /**
+     * Set a request header.
+     * @param name header name
+     * @param value header value
+     * @return this instance
+     */
+    public HTTPMixIn setRequestHeader(String name, String value) {
+        _requestHeaders.put(name, value);
+        return this;
+    }
+    
+    /**
+     * Set the list of request headers.
+     * @param headers request headers in HashMap
+     * @return this instance
+     */
+    public HTTPMixIn setRequestHeaders(HashMap<String,String> headers) {
+        _requestHeaders = headers;
+        return this;
+    }
+    
+    /**
+     * Set a expected response header.
+     * @param name header name
+     * @param value header value
+     * @return this instance
+     */
+    public HTTPMixIn setExpectedHeader(String name, String value) {
+        _expectedHeaders.put(name, value);
+        return this;
+    }
+    
+    /**
+     * Set the list of expected response headers.
+     * @param headers expected response headers in HashMap
+     * @return this instance
+     */
+    public HTTPMixIn setExpectedHeaders(HashMap<String,String> headers) {
+        _expectedHeaders = headers;
+        return this;
+    }
+    
+    /**
+     * Whether to dump the request/response message into log or not.
+     * @param dumpMessages Whether to dump the request/response message into log or not
+     * @return this instance
+     */
+    public HTTPMixIn setDumpMessages(boolean dumpMessages) {
+        _dumpMessages = dumpMessages;
+        return this;
+    }
+    
     @Override
     public void uninitialize() {
         if (_httpClient != null) {
