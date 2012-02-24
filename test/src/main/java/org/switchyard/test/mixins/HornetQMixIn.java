25d70a477bccdca4365cbc07e3c0abbab4a46d0f
Tomohisa Igarashi <tm.igarashi@gmail.com>
2012-02-24 12:51:39 -0500

    SWITCHYARD-637 Add a capability to connect to remote HornetQ server on HornetQMixIn

--- test/src/main/java/org/switchyard/test/mixins/HornetQMixIn.java
+++ test/src/main/java/org/switchyard/test/mixins/HornetQMixIn.java
@@ -23,7 +23,10 @@ package org.switchyard.test.mixins;
 import java.io.ByteArrayInputStream;
 import java.io.ObjectInputStream;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.hornetq.api.core.HornetQBuffer;
 import org.hornetq.api.core.HornetQException;
 import org.hornetq.api.core.TransportConfiguration;
@@ -36,6 +39,8 @@ import org.hornetq.api.core.client.HornetQClient;
 import org.hornetq.api.core.client.ServerLocator;
 import org.hornetq.core.config.Configuration;
 import org.hornetq.core.registry.JndiBindingRegistry;
+import org.hornetq.core.remoting.impl.netty.NettyConnectorFactory;
+import org.hornetq.core.remoting.impl.netty.TransportConstants;
 import org.hornetq.jms.server.embedded.EmbeddedJMS;
 
 /**
@@ -47,35 +52,121 @@ public class HornetQMixIn extends AbstractTestMixIn {
     
     private static final String HORNETQ_CONF_FILE = "hornetq-configuration.xml";
     private static final String HORNETQ_JMS_CONF_FILE = "hornetq-jms.xml";
-    
+
+    private Logger _logger = Logger.getLogger(HornetQMixIn.class);
+    private boolean _startEmbedded;
+    private String _host = TransportConstants.DEFAULT_HOST;
+    private int _port = TransportConstants.DEFAULT_PORT;
+    private String _user = null;
+    private String _passwd = null; 
     private EmbeddedJMS _embeddedJMS;
     private ServerLocator _serverLocator;
     private ClientSessionFactory _clientSessionFactory;
     private ClientSession _clientSession;
 
+    /**
+     * Default constructor.
+     */
+    public HornetQMixIn() {
+        this(true);
+    }
+    
+    /**
+     * Constructor.
+     * @param embedded true if you want to start embedded HornetQ server,
+     * or false if you want to connect to remote HornetQ server.
+     */
+    public HornetQMixIn(boolean embedded) {
+        _startEmbedded = embedded;
+    }
+    
     @Override
     public void initialize() {
-        _embeddedJMS = new EmbeddedJMS();
-        _embeddedJMS.setConfigResourcePath(HORNETQ_CONF_FILE);
-        _embeddedJMS.setJmsConfigResourcePath(HORNETQ_JMS_CONF_FILE);
+        if (_startEmbedded) {
+            _embeddedJMS = new EmbeddedJMS();
+            _embeddedJMS.setConfigResourcePath(HORNETQ_CONF_FILE);
+            _embeddedJMS.setJmsConfigResourcePath(HORNETQ_JMS_CONF_FILE);
+            try {
+                _embeddedJMS.setRegistry(new JndiBindingRegistry());
+                _embeddedJMS.start();
+                _serverLocator = HornetQClient.createServerLocatorWithoutHA(getTransports(getConfiguration()));
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            Map<String, Object> params = new HashMap<String,Object>();
+            params.put(TransportConstants.HOST_PROP_NAME, _host);
+            params.put(TransportConstants.PORT_PROP_NAME, _port);
+            _serverLocator = HornetQClient.createServerLocatorWithoutHA(new TransportConfiguration(NettyConnectorFactory.class.getName(), params));
+        }
+        
         try {
-            _embeddedJMS.setRegistry(new JndiBindingRegistry());
-            _embeddedJMS.start();
-            _serverLocator = HornetQClient.createServerLocatorWithoutHA(getTransports(getConfiguration()));
-            _clientSessionFactory =  _serverLocator.createSessionFactory();
-            _clientSession = _clientSessionFactory.createSession();
+            _clientSessionFactory = _serverLocator.createSessionFactory();
+            _clientSession = _clientSessionFactory.createSession(
+                    _user,
+                    _passwd,
+                    false,
+                    true,
+                    true,
+                    _serverLocator.isPreAcknowledge(),
+                    _serverLocator.getAckBatchSize());
             _clientSession.start();
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Set host address to connect.
+     * @param host address
+     * @return this instance
+     */
+    public HornetQMixIn setHost(String host) {
+        _host = host;
+        return this;
+    }
+    
+    /**
+     * Set port number to connect.
+     * @param port port number
+     * @return this instance
+     */
+    public HornetQMixIn setPort(int port) {
+        _port = port;
+        return this;
+    }
+    
+    /**
+     * Set user to connect.
+     * @param user user
+     * @return this instance
+     */
+    public HornetQMixIn setUser(String user) {
+        _user = user;
+        return this;
+    }
+    
+    /**
+     * Set password to connect.
+     * @param passwd password
+     * @return this instance
+     */
+    public HornetQMixIn setPassword(String passwd) {
+        _passwd = passwd;
+        return this;
     }
     
     /**
      * Gets the HornetQ {@link Configuration}.
      * 
      * @return Configuration the HornetQ configuration used for this Embedded Server.
+     * Return null if this HornetQMixIn instance is initialized for remote HornetQ Server.
      */
     public Configuration getConfiguration() {
+        if (_embeddedJMS == null) {
+            return null;
+        }
+
         return _embeddedJMS.getHornetQServer().getConfiguration();
     }
     
@@ -90,7 +181,10 @@ public class HornetQMixIn extends AbstractTestMixIn {
             HornetQMixIn.closeSessionFactory(_clientSessionFactory);
             HornetQMixIn.closeSession(_clientSession);
             HornetQMixIn.closeServerLocator(_serverLocator);
-            _embeddedJMS.stop();
+            
+            if (_embeddedJMS != null) {
+                _embeddedJMS.stop();
+            }
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
@@ -107,6 +201,28 @@ public class HornetQMixIn extends AbstractTestMixIn {
             return _clientSession;
         }
         throw new IllegalStateException("The ClientSession has not been created. Please check the logs for errors.");
+    }
+    
+    /**
+     * Close the existing ClientSession if exists and create new one.
+     * @return ClientSession instance
+     */
+    public ClientSession createClientSession() {
+        closeSession(_clientSession);
+        try {
+            _clientSession = _clientSessionFactory.createSession(
+                    _user,
+                    _passwd,
+                    false,
+                    true,
+                    true,
+                    _serverLocator.isPreAcknowledge(),
+                    _serverLocator.getAckBatchSize());
+            _clientSession.start();
+            return _clientSession;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
     
     /**
@@ -141,8 +257,18 @@ public class HornetQMixIn extends AbstractTestMixIn {
         byte[] bytes = new byte[msg.getBodySize()];
         final HornetQBuffer bodyBuffer = msg.getBodyBuffer();
         bodyBuffer.readBytes(bytes);
-        final ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes));
-        return in.readObject();
+
+        Object result = null;
+        try {
+            final ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes));
+            result = in.readObject();
+        } catch (Exception e) {
+            _logger.warn("Caught an Exception during deserializing object. Then trying to read as String");
+            _logger.debug("",e);
+            result = new String(bytes);
+        }
+        
+        return result;
     }
 
     /**
