17bd3606d3cb117ad490dc3849624196047b43a3
David Ward <david@errantepiphany.com>
2012-03-06 08:36:20 -0500

    SWITCHYARD-640: Create web app demo that showcases switchyard bpm task api

---
+++ jboss-as7/tests/src/test/java/org/switchyard/test/quickstarts/demo/HelpDeskWebAppDemoQuickstartTest.java
@@ -0,0 +1,49 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.switchyard.test.quickstarts.demo;
+
+import junit.framework.Assert;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.switchyard.test.ArquillianUtil;
+
+/**
+ * The helpdesk webapp demo quickstart test.
+ *
+ * @author David Ward &lt;<a href="mailto:dward@jboss.org">dward@jboss.org</a>&gt; (C) 2011 Red Hat Inc.
+ */
+@RunWith(Arquillian.class)
+public class HelpDeskWebAppDemoQuickstartTest {
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return ArquillianUtil.createWarDemoDeployment("switchyard-quickstart-demo-helpdesk-webapp");
+    }
+
+    @Test
+    public void test() throws Exception {
+    	// Only testing that deployment is successful at this point.
+    	Assert.assertTrue(true);
+    }
+
+}
