613d7941d8b4e56905063efb423d8d5d9f28c2df
Tomohisa Igarashi <tm.igarashi@gmail.com>
2012-05-03 22:48:33 -0400

    SWITCHYARD-708 Add JCA gateway

---
+++ jca-inflow-hornetq/Readme.md
@@ -0,0 +1,33 @@
+Introduction
+============
+This quickstart demonstrates the usage of the JCA Component and it's binding feature,
+by binding to a HornetQ Queue. When a message arrives in this queue the service will be invoked.
+
+JBoss AS 7
+----------
+1. Build the quickstart:
+    mvn clean install
+2. Start JBoss AS 7 in standalone-full mode:
+     ./standalone.sh -server-config standalone-full.xml
+3. Add JMS user using add-user.sh with username=guest, password=guestp, Realm=ApplicationRealm
+    ./add-user.sh
+4. Add a guest role to the user "guest"
+   echo "" >> ${AS7}/standalone/configuration/application-roles.properties
+   echo "guest=guest" >> ${AS7}/standalone/configuration/application-roles.properties
+5. Deploy JMS Queue
+    cp src/test/resources/switchyard-quickstart-hornetq-binding-hornetq-jms.xml ${AS7}/standalone/deployments
+6. Deploy the quickstart
+    cp target/switchyard-quickstarts-hornetq-binding.jar ${AS7}/standalone/deployments
+7. Execute HornetQClient
+    mvn exec:java
+8. Check the server console for output from the service.
+
+Expected Results
+================
+```
+21:51:08,132 INFO  [stdout] (Thread-1 (HornetQ-client-global-threads-1584009536)) Hello there Skippy :-)
+```
+
+## Further Reading
+
+1. [HornetQ Bindings Documentation](https://docs.jboss.org/author/display/SWITCHYARD/JCA+Bindings)
---
+++ jca-inflow-hornetq/src/test/java/org/switchyard/quickstarts/jca/inflow/HornetQClient.java
@@ -0,0 +1,78 @@
+/*
+ * JBoss, Home of Professional Open Source Copyright 2009, Red Hat Middleware
+ * LLC, and individual contributors by the @authors tag. See the copyright.txt
+ * in the distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.switchyard.quickstarts.jca.inflow;
+
+import javax.jms.BytesMessage;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+
+import org.switchyard.test.mixins.HornetQMixIn;
+
+
+/**
+ * HornetQ client that uses HornetQ API to connect to a remote server and
+ * send a message to a queue.
+ *
+ * @author Daniel Bevenius
+ *
+ */
+public final class HornetQClient {
+    
+    private static final String QUEUE = "GreetingServiceQueue";
+    private static final String USER = "guest";
+    private static final String PASSWD = "guestp";
+    
+    /**
+     * Private no-args constructor.
+     */
+    private HornetQClient() {
+    }
+    
+    /**
+     * Only execution point for this application.
+     * @param ignored not used.
+     * @throws Exception if something goes wrong.
+     */
+    public static void main(final String[] args) throws Exception {
+        HornetQMixIn hqMixIn = new HornetQMixIn(false)
+                                    .setUser(USER)
+                                    .setPassword(PASSWD);
+        if (args.length != 1) {
+            System.err.println("ERROR: Use -Dexec.args to pass a name value, e.g. -Dexec.args=\"Skippy\"");
+            return;
+        }
+        
+        hqMixIn.initialize();
+
+        try {
+            final Session session = hqMixIn.createJMSSession();
+            final MessageProducer producer = session.createProducer(HornetQMixIn.getJMSQueue(QUEUE));
+            final BytesMessage message = session.createBytesMessage();
+            message.writeBytes(args[0].getBytes());
+            producer.send(message);
+        
+            System.out.println("Sent message [" + message + "]");
+            Thread.sleep(2000);
+        } finally {
+            hqMixIn.uninitialize();
+        }
+    }
+}
