506c15fec7f25f6f7ff2b0b3bd4ef7dd259feeac
David Ward <david@errantepiphany.com>
2012-10-17 14:05:34 -0400

    SWITCHYARD-1115: ClassNotFoundException when using bpm audit

--- common/rules/src/main/java/org/switchyard/component/common/rules/util/drools/Audits.java
+++ common/rules/src/main/java/org/switchyard/component/common/rules/util/drools/Audits.java
@@ -18,6 +18,7 @@
  */
 package org.switchyard.component.common.rules.util.drools;
 
+import org.apache.commons.lang.text.StrSubstitutor;
 import org.drools.event.KnowledgeRuntimeEventManager;
 import org.drools.logger.KnowledgeRuntimeLogger;
 import org.drools.logger.KnowledgeRuntimeLoggerFactory;
@@ -45,7 +46,7 @@ public final class Audits {
                 type = AuditType.THREADED_FILE;
             }
             String log = Strings.trimToNull(audit.getLog());
-            String fileName = log != null ? log : "event";
+            String fileName = log != null ? StrSubstitutor.replaceSystemProperties(log) : "event";
             Integer interval = audit.getInterval();
             if (interval == null) {
                 interval = Integer.valueOf(1000);
