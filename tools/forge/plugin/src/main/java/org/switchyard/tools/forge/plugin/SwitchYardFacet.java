e15fbe27a46d304bae023c92717a0608d890d703
Tomohisa Igarashi <tm.igarashi@gmail.com>
2012-11-13 15:36:45 -0500

    SWITCHYARD-718 forge tooling requires switchyard.version property in pom for certain paths

--- tools/forge/plugin/src/main/java/org/switchyard/tools/forge/plugin/SwitchYardFacet.java
+++ tools/forge/plugin/src/main/java/org/switchyard/tools/forge/plugin/SwitchYardFacet.java
@@ -117,6 +117,19 @@ public class SwitchYardFacet extends AbstractFacet {
         }
     }
     
+    @Override
+    public boolean isInstalled() {
+        // checking switchyard.xml existence and if the POM has dependency on switchyard-api 
+        if (super.isInstalled() && getSwitchYardConfigFile().exists()) {
+            if (getVersion() == null) {
+                // If it's switchyard project but POM doesn't have ${switchyard.version} property, add it.
+                setVersion(Versions.getSwitchYardVersion());
+            }
+        return true;
+        }
+        return false;
+    }
+    
     /**
      * Save the current SwitchYard configuration model.
      */
