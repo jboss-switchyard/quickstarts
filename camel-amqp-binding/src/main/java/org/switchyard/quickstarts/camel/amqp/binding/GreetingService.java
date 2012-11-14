e863d9c5c8d33fb11812d583a1c7d87848d49cd6
Eduardo de Vera <eduardo.devera@zimory.com>
2012-11-14 21:46:52 +0100

    SWITCHYARD-1158 Quickstart demonstrating the usage of Camel AMQP

---
+++ camel-amqp-binding/src/main/java/org/switchyard/quickstarts/camel/amqp/binding/GreetingService.java
@@ -0,0 +1,11 @@
+package org.switchyard.quickstarts.camel.amqp.binding;
+
+/**
+ * @author: <a href="mailto:eduardo.devera@gmail.com">Eduardo de Vera</a>
+ * Date: 11/11/12
+ * Time: 4:44 PM
+ */
+public interface GreetingService {
+
+    void greet(String name);
+}
