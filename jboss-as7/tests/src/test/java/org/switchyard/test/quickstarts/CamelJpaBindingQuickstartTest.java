a196dff64b07e438baaa96b22a1147b461907214
Lukasz Dywicki <luke@code-house.org>
2012-11-23 17:25:49 +0100

    SWITCHYARD-775 Add quick start for camel-jpa

---
+++ jboss-as7/tests/src/test/java/org/switchyard/test/quickstarts/CamelJpaBindingQuickstartTest.java
@@ -0,0 +1,45 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.switchyard.test.quickstarts;
+
+import java.io.IOException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.switchyard.test.ArquillianUtil;
+
+@RunWith(Arquillian.class)
+public class CamelJpaBindingQuickstartTest {
+
+    @Deployment(testable = false)
+    public static JavaArchive createDeployment() throws IOException {
+        return ArquillianUtil.createJarQSDeployment("switchyard-quickstart-camel-jpa-binding");
+    }
+
+    @Test
+    public void testDeployment() {
+        Assert.assertNotNull("Dummy not null", "");
+    }
+
+
+}
