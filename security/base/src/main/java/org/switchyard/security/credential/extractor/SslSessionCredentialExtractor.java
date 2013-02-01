f374d0b5d47217d1abdab122154afce98f4b4325
Lukasz Dywicki <luke@code-house.org>
2013-02-01 19:50:38 +0100

    SWITCHYARD-1248 Support SSL/TLS for Netty bindings.
    Credentials extractor for SSLSession.

---
+++ security/base/src/main/java/org/switchyard/security/credential/extractor/SslSessionCredentialExtractor.java
@@ -0,0 +1,50 @@
+/* 
+ * JBoss, Home of Professional Open Source 
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved. 
+ * See the copyright.txt in the distribution for a 
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use, 
+ * modify, copy, or redistribute it subject to the terms and conditions 
+ * of the GNU Lesser General Public License, v. 2.1. 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details. 
+ * You should have received a copy of the GNU Lesser General Public License, 
+ * v.2.1 along with this distribution; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package org.switchyard.security.credential.extractor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
+
+import org.switchyard.security.credential.CertificateCredential;
+import org.switchyard.security.credential.ConfidentialityCredential;
+import org.switchyard.security.credential.Credential;
+import org.switchyard.security.credential.PrincipalCredential;
+
+/**
+ * Extractor which will try to obtain {@link Credential} from given {@link SSLSession}.
+ */
+public class SslSessionCredentialExtractor implements CredentialExtractor<SSLSession> {
+
+    @Override
+    public Set<Credential> extract(SSLSession session) {
+        Set<Credential> credentials = new HashSet<Credential>();
+        try {
+            credentials.add(new ConfidentialityCredential(session.isValid()));
+            credentials.add(new PrincipalCredential(session.getPeerPrincipal()));
+            credentials.add(new CertificateCredential(session.getPeerCertificates()[0]));
+        } catch (SSLPeerUnverifiedException e) {
+            throw new RuntimeException("Unable to retrieve credentials from SSL session", e);
+        }
+        return credentials;
+    }
+
+}
