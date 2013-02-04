df1cc55e676e582e18431cba93aba17a3447c02d
Lukasz Dywicki <luke@code-house.org>
2013-02-04 18:17:02 +0100

    SWITCHYARD-1246 Support scripting in Camel routes.

--- camel-service/src/main/java/org/switchyard/quickstarts/camel/service/JavaDSLBuilder.java
+++ camel-service/src/main/java/org/switchyard/quickstarts/camel/service/JavaDSLBuilder.java
@@ -21,13 +21,14 @@ package org.switchyard.quickstarts.camel.service;
 import org.apache.camel.builder.RouteBuilder;
 
 public class JavaDSLBuilder extends RouteBuilder {
-    
+
     public void configure() {
         from("switchyard://JavaDSL")
             .log("Message received in Java DSL Route")
             .log("${body}")
             .split(body(String.class).tokenize("\n"))
-            .filter(body(String.class).startsWith("sally:"))
-            .to("switchyard://XMLService?operationName=acceptMessage");
+            .filter()
+                .groovy("request.getBody().startsWith('sally:')")
+                    .to("switchyard://XMLService?operationName=acceptMessage");
     }
 }
