549aed6a2a6fb8f3c58691345a07b2c0eb8a402e
Magesh Kumar B <mageshbk@jboss.com>
2013-06-04 08:24:34 +0530

    [SWITCHYARD-1205] - Added MTOM/XOP support

---
+++ soap-mtom/Readme.md
@@ -0,0 +1,49 @@
+Introduction
+============
+This quickstart demonstrates the usage of SOAP with MTOM/XOP. It binds
+one SwitchYard service over SOAP/HTTP URL that can be accessed by any SOAP based client.
+It also invokes an external Webservice with the passed in Image binary. The highlight
+here is that XOP envelopes are recognized and expanded with the help of the flag xopExpand="true".
+Since the SOAP Gateway works with SOAPMessages directly the optimization on the service
+and reference outbound side is the responsibility of the user. There is transformer that
+shows how easily the XOP message can be created and attachments added in implementations,
+for example in this via the Camel processors.
+
+```
++-----------------+      +--------------+      +-------------+      +----------------------+
+| http://         | ---- | ImageService | ---- | camel:route | ---- | ImageServiceExternal |
++-----------------+      +--------------+      +-------------+      +----------------------+
+```
+
+![SOAP with MTOM/XOP Quickstart](https://github.com/jboss-switchyard/quickstarts/raw/master/soap-mtom/soap-mtom.jpg)
+
+Running the quickstart
+======================
+
+JBoss AS 7
+----------
+1. Build the quickstart:
+
+        mvn clean install
+
+2. Start JBoss AS 7 in standalone mode:
+
+        ${AS}/bin/standalone.sh --server-config=standalone.xml
+
+3. Deploy the quickstart
+
+        mvn jboss-as:deploy
+
+4. Open a console window and type
+
+        mvn exec:java
+
+5. You should see the following output
+```
+BufferedImage@xxxxx: type = 5 ColorModel: #pixelBits = 24 numComponents = 3 color space = java.awt.color.ICC_ColorSpace@xxxxx transparency = 1 has alpha = false isAlphaPre = false ByteInterleavedRaster: width = 256 height = 256 #numDataElements 3 dataOff[0] = 2
+```
+## Further Reading
+
+1. [SOAP Binding Documentation](https://docs.jboss.org/author/display/SWITCHYARD/SOAP+Bindings)
+2. [SOAP Message Transmission Optimization Mechanism (MTOM)] http://www.w3.org/TR/soap12-mtom/
+3. [XML-binary Optimized Packaging (XOP)] http://www.w3.org/TR/xop10/
---
+++ soap-mtom/src/main/java/org/switchyard/quickstarts/soap/mtom/ServiceTransformers.java
@@ -0,0 +1,99 @@
+/* 
+ * JBoss, Home of Professional Open Source 
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved. 
+ * See the copyright.txt in the distribution for a 
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use, 
+ * modify, copy, or redistribute it subject to the terms and conditions 
+ * of the GNU Lesser General Public License, v. 2.1. 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details. 
+ * You should have received a copy of the GNU Lesser General Public License, 
+ * v.2.1 along with this distribution; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+
+package org.switchyard.quickstarts.soap.mtom;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
+
+import javax.activation.DataSource;
+import javax.imageio.ImageIO;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.switchyard.annotations.Transformer;
+import org.switchyard.common.codec.Base64;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class ServiceTransformers {
+
+    private static String SOAP_RESPONSE_TEMPLATE =
+              "<ns2:imageResponse xmlns:ns2=\"urn:switchyard-quickstart:soap-mtom:1.0\">"
+            + "<xop:Include xmlns:xop=\"http://www.w3.org/2004/08/xop/include\" href=\"cid:%s\"/>"
+            + "</ns2:imageResponse>";
+
+    private static String SOAP_EXTERNAL_REQUEST_TEMPLATE =
+              "<ns2:image xmlns:ns2=\"urn:switchyard-quickstart:soap-mtom-external:1.0\">"
+            + "<xop:Include xmlns:xop=\"http://www.w3.org/2004/08/xop/include\" href=\"cid:%s\"/>"
+            + "</ns2:image>";
+
+    private static String SOAP_EXTERNAL_RESPONSE_TEMPLATE =
+              "<ns2:imageResponse xmlns:ns2=\"urn:switchyard-quickstart:soap-mtom-external:1.0\">"
+            + "<xop:Include xmlns:xop=\"http://www.w3.org/2004/08/xop/include\" href=\"cid:%s\"/>"
+            + "</ns2:imageResponse>";
+
+    @Transformer(from = "{urn:switchyard-quickstart:soap-mtom:1.0}image")
+    public Image transformInternalRequest(Element soap) throws Exception {
+        String imageString = soap.getTextContent();
+        return ImageIO.read(new ByteArrayInputStream(Base64.decode(imageString)));
+    }
+
+    @Transformer(to = "{urn:switchyard-quickstart:soap-mtom-external:1.0}image")
+    public Element transformInternaltoExternalRequest(String imageName) throws Exception {
+        return toElement(String.format(SOAP_EXTERNAL_REQUEST_TEMPLATE, imageName));
+    }
+
+    @Transformer(from = "{urn:switchyard-quickstart:soap-mtom-external:1.0}image")
+    public Image transformExternalRequest(Element soap) throws Exception {
+        String imageString = soap.getTextContent();
+        return ImageIO.read(new ByteArrayInputStream(Base64.decode(imageString)));
+    }
+
+    @Transformer(to = "{urn:switchyard-quickstart:soap-mtom-external:1.0}imageResponse")
+    public Element transformExternaltoInternalResponse(String imageName) throws Exception {
+        return toElement(String.format(SOAP_EXTERNAL_RESPONSE_TEMPLATE, imageName));
+    }
+
+    @Transformer(from = "{urn:switchyard-quickstart:soap-mtom-external:1.0}imageResponse")
+    public Image transformExternalResponse(Element soap) throws Exception {
+        String imageString = soap.getTextContent();
+        return ImageIO.read(new ByteArrayInputStream(Base64.decode(imageString)));
+    }
+
+    @Transformer(to = "{urn:switchyard-quickstart:soap-mtom:1.0}imageResponse")
+    public Element transformInternalResponse(String imageName) throws Exception {
+        return toElement(String.format(SOAP_RESPONSE_TEMPLATE, imageName));
+    }
+
+    private Element toElement(String xml) {
+        DOMResult dom = new DOMResult();
+        try {
+            TransformerFactory.newInstance().newTransformer().transform(new StreamSource(new StringReader(xml)), dom);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ((Document) dom.getNode()).getDocumentElement();
+    }
+}
