5c611e1ebad9fd5c3686ff9539a61bbac20c04ee
hanneli <hannelita@gmail.com>
2013-09-23 02:06:22 -0300

    Fixing files according to QSTools

--- rules-camel-cbr/src/test/java/org/jboss/quickstarts/fsw/RulesCamelCBRTest.java
+++ rules-camel-cbr/src/test/java/org/jboss/quickstarts/fsw/RulesCamelCBRTest.java
@@ -1,5 +1,8 @@
-/*
- * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors.
+/** 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
