6aa77055e117fc18741ab1e435a870f14c29560c
Tomohisa Igarashi <tm.igarashi@gmail.com>
2013-11-07 20:45:03 +0900

    SWITCHYARD-1817 JAXB transformation fails in jca-inflow quickstarts

--- jca-inflow-hornetq/src/main/java/org/switchyard/quickstarts/jca/inflow/package-info.java
+++ jca-inflow-hornetq/src/main/java/org/switchyard/quickstarts/jca/inflow/package-info.java
@@ -1,2 +1,3 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:switchyard-quickstart:jca-inflow-hornetq:0.1.0")
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:switchyard-quickstart:jca-inflow-hornetq:0.1.0",
+                                     elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.switchyard.quickstarts.jca.inflow;
