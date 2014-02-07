jboss-switchyard/core

SWITCHARD-1904 Fix findbugs issues and provide exceptions

common/core/src/main/java/org/switchyard/common/lang/Strings.java
@@ -78,15 +78,15 @@ public final class Strings {
      * @return The repeated String.
      */
     public static String repeat(String str, int repeatCount) {
-        String repeated = "";
+        StringBuffer repeated = new StringBuffer();
         if (str.isEmpty() || repeatCount <= 0) {
-            return repeated;
+            return repeated.toString();
         }
 
         do {
-            repeated += str;
+            repeated.append(str);
         } while (--repeatCount > 0);
-        return repeated;
+        return repeated.toString();
     }
 
     /**

config/src/main/java/org/switchyard/config/model/composite/v1/PolicyConfig.java
@@ -58,11 +58,11 @@ public final class PolicyConfig {
             return;
         }
         Iterator<String> it = requiredSet.iterator();
-        String requires = it.next();
+        StringBuffer requires = new StringBuffer(it.next());
         while (it.hasNext()) {
-            requires += " " + it.next();
+            requires.append(" " + it.next());
         }
         
-        model.getModelConfiguration().setAttribute(REQUIRES, requires);
+        model.getModelConfiguration().setAttribute(REQUIRES, requires.toString());
     }
 }

runtime/src/main/java/org/switchyard/handlers/PolicyHandler.java
@@ -54,11 +54,11 @@ public class PolicyHandler extends BaseHandler {
             if (!required.isEmpty()) {
                 // Required policies are missing.  Format the list for fault message.
                 Iterator<Policy> missing = required.iterator();
-                String requires = missing.next().getName();
+                StringBuffer requires = new StringBuffer(missing.next().getName());
                 while (missing.hasNext()) {
-                    requires += " " + missing.next().getName();
+                    requires.append(" " + missing.next().getName());
                 }
-                throw RuntimeMessages.MESSAGES.requiredPolicesNeeded(requires);
+                throw RuntimeMessages.MESSAGES.requiredPolicesNeeded(requires.toString());
             }
         }
     }

runtime/src/main/java/org/switchyard/internal/DefaultHandlerChain.java
@@ -154,12 +154,12 @@ public class DefaultHandlerChain implements HandlerChain {
                 }
             }
         } catch (HandlerException handlerEx) {
-            String errorMsg = handlerEx.getClass().getName() + " was thrown by handler(" + handlerRef.getName() + "): " + handlerEx.getMessage();
+            StringBuffer errorMsg = new StringBuffer(handlerEx.getClass().getName() + " was thrown by handler(" + handlerRef.getName() + "): " + handlerEx.getMessage());
             Throwable cause = handlerEx;
             while ((cause = cause.getCause()) != null) {
-                errorMsg += " --- Caused by " + cause.getClass().getName() + ": " + cause.getMessage();
+                errorMsg.append(" --- Caused by " + cause.getClass().getName() + ": " + cause.getMessage());
             }
-            _logger.error(errorMsg);
+            _logger.error(errorMsg.toString());
             _logger.debug("", handlerEx);
 
             Message faultMessage = exchange.createMessage().setContent(handlerEx);

tools/forge/plugin/src/main/java/org/switchyard/tools/forge/plugin/TemplateResource.java
@@ -103,16 +103,16 @@ public class TemplateResource {
         
         packageName(packageName);
 
-        String destDir = ".." + File.separator + ".." + File.separator
+        StringBuffer destDir = new StringBuffer(".." + File.separator + ".." + File.separator
             + File.separator + (isTest ? "test" : "main")
-            + File.separator + "java";
+            + File.separator + "java");
         if (packageName != null && packageName.length() > 0) {
             for (String pkgDir : packageName.split("\\.")) {
-                destDir += File.separator + pkgDir;
-            }
+                destDir.append(File.separator + pkgDir);
+            }            
         }
         String destFile = className + ".java";
-        writeResource(resources.getResource(destDir + File.separator + destFile));
+        writeResource(resources.getResource(destDir.toString() + File.separator + destFile));
         
         return destFile;
     }

jboss-switchyard/quickstarts

common/core/src/main/java/org/switchyard/common/lang/Strings.java
@@ -78,15 +78,15 @@ public final class Strings {
      * @return The repeated String.
      */
     public static String repeat(String str, int repeatCount) {
-        String repeated = "";
+        StringBuilder repeated = new StringBuilder();
         if (str.isEmpty() || repeatCount <= 0) {
-            return repeated;
+            return repeated.toString();
         }
 
         do {
-            repeated += str;
+            repeated.append(str);
         } while (--repeatCount > 0);
-        return repeated;
+        return repeated.toString();
     }
 
     /**

config/src/main/java/org/switchyard/config/model/composite/v1/PolicyConfig.java
@@ -58,11 +58,11 @@ public final class PolicyConfig {
             return;
         }
         Iterator<String> it = requiredSet.iterator();
-        String requires = it.next();
+        StringBuilder requires = new StringBuilder(it.next());
         while (it.hasNext()) {
-            requires += " " + it.next();
+            requires.append(" " + it.next());
         }
         
-        model.getModelConfiguration().setAttribute(REQUIRES, requires);
+        model.getModelConfiguration().setAttribute(REQUIRES, requires.toString());
     }
 }

runtime/src/main/java/org/switchyard/handlers/PolicyHandler.java
@@ -54,11 +54,11 @@ public class PolicyHandler extends BaseHandler {
             if (!required.isEmpty()) {
                 // Required policies are missing.  Format the list for fault message.
                 Iterator<Policy> missing = required.iterator();
-                String requires = missing.next().getName();
+                StringBuilder requires = new StringBuilder(missing.next().getName());
                 while (missing.hasNext()) {
-                    requires += " " + missing.next().getName();
+                    requires.append(" " + missing.next().getName());
                 }
-                throw RuntimeMessages.MESSAGES.requiredPolicesNeeded(requires);
+                throw RuntimeMessages.MESSAGES.requiredPolicesNeeded(requires.toString());
             }
         }
     }

runtime/src/main/java/org/switchyard/internal/DefaultHandlerChain.java
@@ -154,12 +154,12 @@ public class DefaultHandlerChain implements HandlerChain {
                 }
             }
         } catch (HandlerException handlerEx) {
-            String errorMsg = handlerEx.getClass().getName() + " was thrown by handler(" + handlerRef.getName() + "): " + handlerEx.getMessage();
+            StringBuilder errorMsg = new StringBuilder(handlerEx.getClass().getName() + " was thrown by handler(" + handlerRef.getName() + "): " + handlerEx.getMessage());
             Throwable cause = handlerEx;
             while ((cause = cause.getCause()) != null) {
-                errorMsg += " --- Caused by " + cause.getClass().getName() + ": " + cause.getMessage();
+                errorMsg.append(" --- Caused by " + cause.getClass().getName() + ": " + cause.getMessage());
             }
-            _logger.error(errorMsg);
+            _logger.error(errorMsg.toString());
             _logger.debug("", handlerEx);
 
             Message faultMessage = exchange.createMessage().setContent(handlerEx);

tools/forge/plugin/src/main/java/org/switchyard/tools/forge/plugin/TemplateResource.java
@@ -103,16 +103,16 @@ public class TemplateResource {
         
         packageName(packageName);
 
-        String destDir = ".." + File.separator + ".." + File.separator
+        StringBuilder destDir = new StringBuilder(".." + File.separator + ".." + File.separator
             + File.separator + (isTest ? "test" : "main")
-            + File.separator + "java";
+            + File.separator + "java");
         if (packageName != null && packageName.length() > 0) {
             for (String pkgDir : packageName.split("\\.")) {
-                destDir += File.separator + pkgDir;
-            }
+                destDir.append(File.separator + pkgDir);
+            }            
         }
         String destFile = className + ".java";
-        writeResource(resources.getResource(destDir + File.separator + destFile));
+        writeResource(resources.getResource(destDir.toString() + File.separator + destFile));
         
         return destFile;
     }