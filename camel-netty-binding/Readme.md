abe5fcfcc4ad526ec683a3287168fef96d2f1591
David Ward <dward@redhat.com>
2014-04-02 01:36:03 -0400

    SWITCHYARD-2031: Please allow public API access to Lite version of SecurityContext

--- camel-netty-binding/Readme.md
+++ camel-netty-binding/Readme.md
@@ -22,7 +22,9 @@ JBoss AS 7
 
 4. Check the server console for output from the service:
 
-    'Default: Hello there <your text>';
+        :: DefaultGreetingService :: Hello <your text>! (caller principal=null, in roles? 'friend'=false 'enemy'=false)
+
+   (Because the DefaultGreetingService is not secured, you will see the null principal, and false for both security roles.)
 
 To test TCP: 
 
@@ -32,7 +34,9 @@ To test TCP:
 
 6. Check the server console for output from the service:
 
-        'Secured: Hello there <your text>';
+        :: SecuredGreetingService :: Hello <your text>! (caller principal=UserPrincipal@1741605094[name=kermit], in roles? 'friend'=true 'enemy'=false)
+
+   (Because the SecuredGreetingService is secured, you will see the not-null principal, and true for the expected security role.)
 
 7. Undeploy the quickstart:
 
--- demos/policy-security-wss-username/src/main/java/org/switchyard/quickstarts/demo/policy/security/wss/username/WorkServiceBean.java
+++ demos/policy-security-wss-username/src/main/java/org/switchyard/quickstarts/demo/policy/security/wss/username/WorkServiceBean.java
@@ -20,7 +20,11 @@ import static org.switchyard.policy.SecurityPolicy.AUTHORIZATION;
 import static org.switchyard.policy.SecurityPolicy.CLIENT_AUTHENTICATION;
 import static org.switchyard.policy.SecurityPolicy.CONFIDENTIALITY;
 
+import javax.inject.Inject;
+
 import org.apache.log4j.Logger;
+import org.switchyard.Exchange;
+import org.switchyard.ExchangeSecurity;
 import org.switchyard.annotations.Requires;
 import org.switchyard.component.bean.Service;
 
@@ -29,11 +33,17 @@ import org.switchyard.component.bean.Service;
 public class WorkServiceBean implements WorkService {
 
     private static final Logger LOGGER = Logger.getLogger(WorkServiceBean.class);
+    private static final String MSG = ":: WorkService :: Received work command => %s (caller principal=%s, in roles? 'friend'=%s 'enemy'=%s)";
+
+    @Inject
+    private Exchange exchange;
 
     @Override
     public WorkAck doWork(Work work) {
         String cmd = work.getCommand();
-        LOGGER.info(":: WorkService :: Received work command => " + cmd);
+        ExchangeSecurity es = exchange.getSecurity();
+        String msg = String.format(MSG, cmd, es.getCallerPrincipal(), es.isCallerInRole("friend"), es.isCallerInRole("enemy"));
+        LOGGER.info(msg);
         return new WorkAck().setCommand(cmd).setReceived(true);
     }
 
