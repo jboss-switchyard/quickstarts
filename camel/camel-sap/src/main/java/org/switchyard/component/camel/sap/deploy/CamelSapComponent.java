jboss-switchyard/components

SWITCHYARD-1976 Add support for Camel SAP component

camel/camel-sap/src/main/java/org/switchyard/component/camel/sap/deploy/CamelSapComponent.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.switchyard.component.camel.sap.deploy;
+
+import org.switchyard.component.camel.common.deploy.BaseBindingComponent;
+import org.switchyard.component.camel.sap.model.v1.V1CamelSapBindingModel;
+
+/**
+ * Camel sap component.
+ */
+public class CamelSapComponent extends BaseBindingComponent {
+
+    /**
+     * Creates new component.
+     */
+    public CamelSapComponent() {
+        super("CamelSapComponent", V1CamelSapBindingModel.SAP);
+    }
+
+}

jboss-switchyard/quickstarts

camel/camel-sap/src/main/java/org/switchyard/component/camel/sap/deploy/CamelSapComponent.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.switchyard.component.camel.sap.deploy;
+
+import org.switchyard.component.camel.common.deploy.BaseBindingComponent;
+import org.switchyard.component.camel.sap.model.v2.V2CamelSapBindingModel;
+
+/**
+ * Camel sap component.
+ */
+public class CamelSapComponent extends BaseBindingComponent {
+
+    /**
+     * Creates new component.
+     */
+    public CamelSapComponent() {
+        super("CamelSapComponent", V2CamelSapBindingModel.SAP);
+    }
+
+}