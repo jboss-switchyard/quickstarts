mageshbk/components

[SWITCHYARD-2004] - Added Camel CXF component

camel/camel-cxf/src/main/java/org/switchyard/component/camel/cxf/deploy/CamelCxfComponent.java
@@ -1,0 +1,32 @@
+/* 
+ * Copyright 2014 Red Hat Inc. and/or its affiliates and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.switchyard.component.camel.cxf.deploy;
+
+import org.switchyard.component.camel.common.deploy.BaseBindingComponent;
+import org.switchyard.component.camel.cxf.model.v1.V1CamelCxfBindingModel;
+
+/**
+ * Camel cxf  component.
+ */
+public class CamelCxfComponent extends BaseBindingComponent {
+
+    /**
+     * Creates new component.
+     */
+    public CamelCxfComponent() {
+        super("CamelCxfComponent", V1CamelCxfBindingModel.CXF);
+    }
+
+}
+

jboss-switchyard/quickstarts

camel/camel-cxf/src/main/java/org/switchyard/component/camel/cxf/deploy/CamelCxfComponent.java
@@ -1,0 +1,32 @@
+/* 
+ * Copyright 2014 Red Hat Inc. and/or its affiliates and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.switchyard.component.camel.cxf.deploy;
+
+import org.switchyard.component.camel.common.deploy.BaseBindingComponent;
+import org.switchyard.component.camel.cxf.model.v2.V2CamelCxfBindingModel;
+
+/**
+ * Camel cxf  component.
+ */
+public class CamelCxfComponent extends BaseBindingComponent {
+
+    /**
+     * Creates new component.
+     */
+    public CamelCxfComponent() {
+        super("CamelCxfComponent", V2CamelCxfBindingModel.CXF);
+    }
+
+}
+