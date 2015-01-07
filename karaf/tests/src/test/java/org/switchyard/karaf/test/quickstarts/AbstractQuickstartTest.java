0ffb1d021bee67edeab230c2091e769ee7080bea
Tom Cunningham <tcunning@redhat.com>
2015-01-07 11:33:03 -0500

    [SWITCHYARD-2066 - Included karaf ftp, ftps and sftp test]

--- karaf/tests/src/test/java/org/switchyard/karaf/test/quickstarts/AbstractQuickstartTest.java
+++ karaf/tests/src/test/java/org/switchyard/karaf/test/quickstarts/AbstractQuickstartTest.java
@@ -38,6 +38,7 @@ import org.ops4j.pax.exam.TestProbeBuilder;
 import org.ops4j.pax.exam.TestProbeProvider;
 import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
 import org.ops4j.pax.exam.spi.PaxExamRuntime;
+import org.osgi.framework.Constants;
 import org.switchyard.karaf.test.quickstarts.PhaseListener.Phase;
 
 public abstract class AbstractQuickstartTest {
@@ -90,6 +91,7 @@ public abstract class AbstractQuickstartTest {
             // install the probe
             TestProbeBuilder probeBuilder = system.createProbe();
             time = post(phaseListener, CREATE_PROBE, time);
+            probeBuilder.setHeader(Constants.DYNAMICIMPORT_PACKAGE, "*,org.apache.felix.service.*;status=provisional");
             probeBuilder.addTests(probeClass, probeClass.getMethods());
             time = post(phaseListener, ADD_TESTS, time);
             testProbe = probeBuilder.build();
@@ -146,6 +148,21 @@ public abstract class AbstractQuickstartTest {
                 configureConsole().ignoreLocalConsole().ignoreRemoteShell(),
                 editConfigurationFileExtend("etc/config.properties", "org.osgi.framework.system.packages.extra",
                         "sun.misc"),
+<<<<<<< HEAD
+=======
+                editConfigurationFilePut("etc/system.properties", "org.switchyard.component.http.standalone.port",
+                        "8181"),
+                editConfigurationFilePut("etc/system.properties", "org.switchyard.component.soap.client.port",
+                        "8181/cxf"),
+                editConfigurationFilePut("etc/system.properties", "org.switchyard.component.soap.standalone.port",
+                        "8181"),
+                editConfigurationFilePut("etc/system.properties", "org.switchyard.component.camel.ftps.storefile",
+                        "../../../test-classes/quickstarts/camel-ftp-binding/ftpclient.jks"),
+                editConfigurationFilePut("etc/system.properties", "org.switchyard.component.camel.sftp.knownhosts",
+                        "../../../test-classes/quickstarts/camel-ftp-binding/known_hosts_sftp"),
+                editConfigurationFilePut("etc/system.properties", "org.switchyard.component.camel.sftp.keyfile",
+                        "../../../test-classes/quickstarts/camel-ftp-binding/id_sftp_rsa"),
+>>>>>>> 92ba74e... [SWITCHYARD-2066 - Included karaf ftp, ftps and sftp test]
                 editConfigurationFilePut(
                         "etc/org.ops4j.pax.url.mvn.cfg",
                         "org.ops4j.pax.url.mvn.repositories",
