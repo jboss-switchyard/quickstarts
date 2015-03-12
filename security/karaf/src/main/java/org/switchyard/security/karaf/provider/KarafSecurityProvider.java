51c978f98dc8e8bb66deb5e9666de9ab293c2751
dvirgiln <david.virgil.naranjo@gmail.com>
2015-03-12 11:52:03 +0100

    SWITCHYARD-2547 demo-policy-security-basic - Required policy "authorization" is not provided on karaf

--- security/karaf/src/main/java/org/switchyard/security/karaf/provider/KarafSecurityProvider.java
+++ security/karaf/src/main/java/org/switchyard/security/karaf/provider/KarafSecurityProvider.java
@@ -1,23 +1,32 @@
 /*
- * Copyright 2014 Red Hat Inc. and/or its affiliates and other contributors.
+ * Copyright 2015 JBoss Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package org.switchyard.security.karaf.provider;
 
+import java.security.Principal;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+
 import org.apache.karaf.jaas.boot.ProxyLoginModule;
+import org.apache.karaf.jaas.boot.principal.GroupPrincipal;
 import org.switchyard.ServiceSecurity;
 import org.switchyard.security.context.SecurityContext;
 import org.switchyard.security.provider.DefaultSecurityProvider;
 
+// TODO: Auto-generated Javadoc
 /**
  * KarafSecurityProvider.
  *
@@ -54,4 +63,47 @@ public class KarafSecurityProvider extends DefaultSecurityProvider {
         super.clear(serviceSecurity, securityContext);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean checkRolesAllowed(ServiceSecurity serviceSecurity, SecurityContext securityContext) {
+        Set<String> rolesAllowed = serviceSecurity.getRolesAllowed();
+        if (rolesAllowed.isEmpty()) {
+            return true;
+        }
+        String securityDomain = serviceSecurity.getSecurityDomain();
+        for (String roleName : rolesAllowed) {
+            boolean isInRole = this.isCallerInRole(securityContext, roleName, securityDomain);
+            if (isInRole) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if is caller in role.
+     *
+     * @param securityContext
+     *            the security context
+     * @param roleName
+     *            the role name
+     * @param securityDomain
+     *            the security domain
+     * @return true, if is caller in role
+     */
+    public boolean isCallerInRole(SecurityContext securityContext, String roleName, String securityDomain) {
+        Subject subject = securityContext.getSubject(securityDomain, false);
+        if (subject != null) {
+            for (Principal principal : subject.getPrincipals()) {
+                if (principal instanceof GroupPrincipal) {
+                    if (principal.getName().equalsIgnoreCase(roleName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
