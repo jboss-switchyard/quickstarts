f3313f7ae58c291749cc393418916ae9b5935628
Tomohisa Igarashi <tm.igarashi@gmail.com>
2015-03-17 07:26:41 +0900

    SWITCHYARD-2615 Update README for demos/cluster,transaction-propagation
    Also added logging into CreditCheckBean which is removed by ENTESB-2431

--- demos/cluster/credit/src/main/java/org/switchyard/quickstarts/demo/cluster/CreditCheckBean.java
+++ demos/cluster/credit/src/main/java/org/switchyard/quickstarts/demo/cluster/CreditCheckBean.java
@@ -1,16 +1,21 @@
 package org.switchyard.quickstarts.demo.cluster;
 
+import org.jboss.logging.Logger;
 import org.switchyard.component.bean.Service;
 
 @Service(CreditCheck.class)
 public class CreditCheckBean implements CreditCheck {
 
+    private Logger _log = Logger.getLogger(CreditCheckBean.class);
+
     @Override
     public Application checkCredit(Offer offer) {
         if (offer.getApplication().getCreditScore() >= 600) {
             offer.getApplication().setApproved(true);
+            _log.info("Credit Service : Approving credit for " + offer.getApplication().getName());
         } else {
             offer.getApplication().setApproved(false);
+            _log.info("Credit Service : Denying credit for " + offer.getApplication().getName());
         }
         return offer.getApplication();
     }
